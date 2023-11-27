d2a60831d963ee539964e31e81b3539b173a2920
Mehedi Hassan <mh93075@gmail.com>
2023-11-27 07:20:23 +0600

    Set  token to the local storafe and send it backend by interceptors

--- src/Component/AuthProvider/AuthProvider.jsx
+++ src/Component/AuthProvider/AuthProvider.jsx
@@ -1,52 +1,75 @@
 import { createContext, useEffect, useState } from "react";
-import {  GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import auth from "./Frebase/firebase";
-
+import useAxiosPublic from "./useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
 const provider = new GoogleAuthProvider();
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const  axiosPublic = useAxiosPublic();
 
-    const createUser = (email, password) =>{
-        setLoading(true);
-        return createUserWithEmailAndPassword(auth, email, password);
-        
-    }
-    const signInUser = (email, password) =>{
-        setLoading(true);
-        return signInWithEmailAndPassword(auth, email, password);
-    }
-    const signInWithGoogle = () =>{
-        setLoading(true);
-        return signInWithPopup(auth, provider);
-    }
-    const logOut =()=>{
-        setLoading(true);
-        return signOut(auth);
-    }
-    useEffect(()=>{
-        const unSubscribe = onAuthStateChanged(auth, currentUser=>{
-            const userEmail = currentUser?.email || user?.email;
-            const loggedUser = {email: userEmail}
-            setUser(currentUser);
-            console.log(currentUser);
-            setLoading(false);
-            
-        })
-        return () =>{
-            unSubscribe();
-        }
-    },[])
-    const authInfo = {user, loading, createUser, signInUser, signInWithGoogle, logOut};
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = { email: userEmail };
+      setUser(currentUser);
+      console.log(currentUser);
+      if (currentUser) {
+        // get token and store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+        localStorage.removeItem("access-token");
+      }
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signInUser,
+    signInWithGoogle,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/Component/AuthProvider/useAxiosSecure.jsx
+++ src/Component/AuthProvider/useAxiosSecure.jsx
@@ -3,32 +3,44 @@ import { useEffect } from "react";
 import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
 
-
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000/',
-    withCredentials: true
+  baseURL: "http://localhost:5000/",
+  withCredentials: true,
 });
 
 const useAxiosSecure = () => {
-    const { logOut } = useAuth();
-    const navigate = useNavigate();
-    useEffect(() => {
-        axiosSecure.interceptors.response.use(res => {
-            return res;
-        }, error => {
-            console.log('error tracked in the interceptor', error.response)
-            if (error.response.status === 401 || error.response.status === 403) {
-                console.log('logout the user')
-                logOut()
-                    .then(() => { 
-                        navigate('/login')
-                    })
-                    .catch(error => console.log(error))
-            }
-        })
-    }, [])
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
 
-    return axiosSecure;
+  // request interceptor to add authorization header for every secure call to teh api
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('access-token')
+    console.log('request stopped by interceptors', token)
+    config.headers.authorization = `Bearer ${token}`;
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+  axiosSecure.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    (error) => {
+      console.log("error tracked in the interceptor", error.response);
+      if (error.response.status === 401 || error.response.status === 403) {
+        console.log("logout the user");
+        logOut()
+          .then(() => {
+            navigate("/login");
+          })
+          .catch((error) => console.log(error));
+      }
+    }
+  );
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
